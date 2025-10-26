dba4b336beb63a885ff848f1457cc5100d4848c1
Mariana Louro <mmarqueslouro@gmail.com>
2025-10-26 18:38:37 -0300

    Validação de formulário e criação de tarefa

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -1,12 +1,50 @@
 import { PlayCircleIcon } from "lucide-react";
+import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import type { TaskModel } from "../../models/TaskModel";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log("deu certo");
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert("Digite o nome da tarefa");
+      return;
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: "workTime",
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState((prevState) => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: "00:00",
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
 
   return (
@@ -17,6 +55,7 @@ export function MainForm() {
           id="input"
           type="text"
           placeholder="Digite"
+          ref={taskNameInput}
         />
       </div>
 
