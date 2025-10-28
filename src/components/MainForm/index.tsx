8270e3dc53541b44638f1e431083915e382e49c4
Mariana Louro <mmarqueslouro@gmail.com>
2025-10-27 22:16:08 -0300

    ajustando o nextCycle

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,13 +2,20 @@ import { PlayCircleIcon } from "lucide-react";
 import { useRef } from "react";
 import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import type { TaskModel } from "../../models/TaskModel";
+import { getNextCycle } from "../../utils/getNextCycle";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
 
 export function MainForm() {
-  const { setState } = useTaskContext();
+  const { state, setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
+
+  // Ciclos
+
+  const nextCycle = getNextCycle(state.currentCycle);
+  const nextCycleType = getNextCycleType(nextCycle);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
@@ -28,9 +35,11 @@ export function MainForm() {
       startDate: Date.now(),
       completeDate: null,
       interruptDate: null,
-      duration: 1,
-      type: "workTime",
+      duration: state.config[nextCycleType],
+      type: nextCycleType,
     };
+
+    console.log("🎯 newTask:", state, newTask);
 
     const secondsRemaining = newTask.duration * 60;
 
@@ -39,7 +48,7 @@ export function MainForm() {
         ...prevState,
         config: { ...prevState.config },
         activeTask: newTask,
-        currentCycle: 1,
+        currentCycle: nextCycle,
         secondsRemaining,
         formattedSecondsRemaining: "00:00",
         tasks: [...prevState.tasks, newTask],
@@ -68,7 +77,7 @@ export function MainForm() {
       </div>
 
       <div className="formRow">
-        <DefaultButton icon={<PlayCircleIcon />} />
+        <DefaultButton type="submit" icon={<PlayCircleIcon />} />
       </div>
     </form>
   );
